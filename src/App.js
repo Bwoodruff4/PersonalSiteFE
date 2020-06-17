37968cf1d1669e93c527c77f3ba7edd2126e972f
Bwoodruff4 <blakewoodruff4@gmail.com>
2020-06-16 20:49:10 -0600

    Added nav bar, routing functionality, and some styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import NavBar from './Components/NavBar'
+import Home from './Components/Home'
+import Background from './Components/Background'
+import Experience from './Components/Experience'
+import Articles from './Components/Articles'
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container'
+
+const backgroundURL = "https://images.unsplash.com/photo-1550751827-4bd374c3f58b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+
+const useStyles = makeStyles({
+  background: {
+    backgroundImage: `url(${backgroundURL})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    maxWidth: "100%",
+    height: "100vh",
+    overflow: "hidden",
+    scroll: "no"
+  },
+});
 
 function App() {
+  const classes = useStyles()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar color="primary"/>
+      <Container className={classes.background}>
+      <Grid container spacing={0} direction="column" style={{height: "100vh"}} >
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/Background" exact component={Background} />
+          <Route path="/Experience" exact component={Experience} />
+          <Route path="/Articles" exact component={Articles} />
+        </Switch>
+      </Grid>
+      </Container>
+    </Router>
   );
 }
 
---
+++ src/Components/MenuDrawer.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+// import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+// import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+// import InboxIcon from '@material-ui/icons/MoveToInbox';
+// import MailIcon from '@material-ui/icons/Mail';
+import MenuIcon from '@material-ui/icons/Menu';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+});
+
+export default function TemporaryDrawer() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    leftMenu: false,
+  });
+
+  const toggleDrawer = (anchor,open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+          <ListItemLink primary={"Home"}to={"/"}/>
+        {['Background', 'Experience', 'Projects'].map((text) => (
+          <ListItemLink primary={`${text}`}to={`/${text}`}/>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['Contact'].map((text) => (
+          <ListItem button key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <div>
+      {
+        <React.Fragment key='leftMenu'>
+            <MenuIcon onClick={toggleDrawer('leftMenu', true)} />
+            <Drawer anchor='left' open={state['leftMenu']} onClose={toggleDrawer('leftMenu', false)}>
+                {list('leftMenu')}
+            </Drawer>
+        </React.Fragment>
+      }
+    </div>
+  );
+}
+
+function ListItemLink(props) {
+  const { primary, to } = props;
+
+  const renderLink = React.useMemo(
+    () => React.forwardRef((itemProps, ref) => <Link to={to} ref={ref} {...itemProps} />),
+    [to],
+  );
+
+  return (
+    <li>
+      <ListItem button component={renderLink}>
+        <ListItemText primary={primary} />
+      </ListItem>
+    </li>
+  );
+}
